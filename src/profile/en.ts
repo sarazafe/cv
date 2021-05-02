451e527b6cc64c0fdf141d451508a79f8974bb04
sarazafe <sara@blunder.es>
2021-05-02 13:07:42 +0200

    Refactor: improve the structure of the data

--- src/profile/en.ts
+++ src/profile/en.ts
@@ -1,6 +1,8 @@
 export const EnglishProfile = {
     contact: {
         title: 'Contact',
+        name: 'Sara Zapico Fernández',
+        address: 'Turón, Asturias, España',
         email: 'sarazafe@gmail.com',
         sites: [
             {
@@ -18,102 +20,118 @@ export const EnglishProfile = {
             },
         ],
     },
-    aptitudesTitle: 'Main aptitudes',
-    aptitudes: [
-        'Java',
-        'Javascript/Typescript',
-        'Angular',
-        'React',
-        'HTML',
-        'CSS'
-    ],
-    languagesTitle: 'Languages',
-    languages: [{
-        language: 'English',
-        level: '(Full Professional)'
+    aptitudes: {
+        title: 'Main aptitudes',
+        aptitudes: [
+            'Java',
+            'Javascript/Typescript',
+            'Angular',
+            'React',
+            'HTML',
+            'CSS'
+        ],
     },
-        {
-            language: 'French',
-            level: '(Elementary)'
+    languages: {
+        title: 'Languages',
+        languages: [{
+            language: 'English',
+            level: '(Full Professional)'
         },
-        {
-            language: 'Spanish',
-            level: '(Native or Bilingual)'
-        },
-        {
-            language: 'Asturiano',
-            level: '(Native or Bilingual)'
-        }],
-    certificationsTitle: 'Certifications',
-    certifications: [
-        {
-            name: `Udacity's React Nanodegree`,
-            resources: '../assets/react_nanodegree.pdf'
-        },
-        {
-            name: 'Shaping up with Angular.js',
-        },
-        {
-            name: 'Web Assessment for SAP Message Processor and Operations for Support Consultant',
-        },
-    ],
-    certificationsInProgressTitle: 'Certifications in progress',
-    certificationsInProgress: [
-        {
-            name: 'Epic React',
-            resources: 'https://epicreact.dev/'
-        },
-    ],
-    honorsTitle: 'Honors-Awards',
-    honors: ['Award for the best academic record for the 2009-2011 Master\'s Degree in Web Engineering'],
+            {
+                language: 'French',
+                level: '(Elementary)'
+            },
+            {
+                language: 'Spanish',
+                level: '(Native or Bilingual)'
+            },
+            {
+                language: 'Asturiano',
+                level: '(Native or Bilingual)'
+            }],
+    },
+    certifications: {
+        title: 'Certifications',
+        certifications: [
+            {
+                name: `Udacity's React Nanodegree`,
+                resources: '../assets/react_nanodegree.pdf'
+            },
+            {
+                name: 'Shaping up with Angular.js',
+            },
+            {
+                name: 'Web Assessment for SAP Message Processor and Operations for Support Consultant',
+            },
+        ],
+    },
+    certificationsInProgress: {
+        title: 'Certifications in progress',
+        certificationsInProgress: [
+            {
+                name: 'Epic React',
+                resources: 'https://epicreact.dev/'
+            },
+        ],
+    },
+    honors: {
+        title: 'Honors-Awards',
+        honors: ['Award for the best academic record for the 2009-2011 Master\'s Degree in Web Engineering'],
+    },
     job: 'Software developer',
-    extractTitle: 'Extract',
-    extract: 'Full-stack developer with Java profile in the Back part and CSS, HTML, ' +
-            'Javascript/Typescript and Angular in the Front part. I really like the challenges and learning new ' +
-            'things all the time. As soon as I have a free moment I start to draw and paint and this is, maybe, the ' +
-            'reason because on many occasions I feel that the balance leans towards the Front.',
-    experienceTitle: 'Experience',
-    experience: [
-        {
-            company: 'Empathy.co',
-            role: 'Front-end developer',
-            duration: 'June 2020 - Present'
-        },
-        {
-            company: 'Zapiens Technologies SL',
-            role: 'Program Analyst',
-            duration: 'May 2017 - June 2020'
-        },
-        {
-            company: 'Ricoh España',
-            role: 'Senior Web Programmer',
-            duration: 'April 2015 - May 2017'
-        },
-        {
-            company: 'Nortia Inteligencia de Negocio',
-            role: 'Program Analyst',
-            duration: 'June 2011 - April 2015'
-        },
-        {
-            company: 'Grupo Iris',
-            role: 'Web Programmer',
-            duration: 'September 2010 - June 2011'
-        },
-        {
-            company: 'University of Oviedo',
-            role: 'Webmaster',
-            duration: 'September 2009 - July 2010'
-        }
-    ],
-    educationTitle: 'Education',
-    education: [
-        {
-            place: 'University of Oviedo',
-            degree: 'Master of Web Engineer (2009 - 2011)'
-        },
-        {
-            place: 'University of Oviedo',
-            degree: 'Software Engineer (2005 - 2009)'
-        }
-    ]
+    extract: {
+        title: 'Extract',
+        extract: 'Full-stack developer with Java profile in the Back part and CSS, HTML, ' +
+                'Javascript/Typescript and Angular in the Front part. I really like the challenges and learning new ' +
+                'things all the time. As soon as I have a free moment I start to draw and paint and this is, maybe, the ' +
+                'reason because on many occasions I feel that the balance leans towards the Front.',
+    },
+    experience: {
+        title: 'Experience',
+        experience: [
+            {
+                company: 'Empathy.co',
+                role: 'Front-end developer',
+                duration: 'June 2020 - Present'
+            },
+            {
+                company: 'Zapiens Technologies SL',
+                role: 'Program Analyst',
+                duration: 'May 2017 - June 2020'
+            },
+            {
+                company: 'Ricoh España',
+                role: 'Senior Web Programmer',
+                duration: 'April 2015 - May 2017'
+            },
+            {
+                company: 'Nortia Inteligencia de Negocio',
+                role: 'Program Analyst',
+                duration: 'June 2011 - April 2015'
+            },
+            {
+                company: 'Grupo Iris',
+                role: 'Web Programmer',
+                duration: 'September 2010 - June 2011'
+            },
+            {
+                company: 'University of Oviedo',
+                role: 'Webmaster',
+                duration: 'September 2009 - July 2010'
+            }
+        ],
+    },
+    education: {
+        title: 'Education',
+        education: [
+            {
+                place: 'University of Oviedo',
+                degree: 'Master of Web Engineer (2009 - 2011)'
+            },
+            {
+                place: 'University of Oviedo',
+                degree: 'Software Engineer (2005 - 2009)'
+            }
+        ],
+    },
 };
--- src/profile/es.ts
+++ src/profile/es.ts
@@ -1,6 +1,8 @@
 export const SpanishProfile = {
     contact: {
         title: 'Contactar',
+        name: 'Sara Zapico Fernández',
+        address: 'Turón, Asturias, Spain',
         email: 'sarazafe@gmail.com',
         sites: [
             {
@@ -18,101 +20,118 @@ export const SpanishProfile = {
             },
         ],
     },
-    attitudesTitle: 'Principales aptitudes',
-    attitudes: [
-        'Java',
-        'Javascript/Typescript',
-        'Angular',
-        'React',
-        'HTML',
-        'CSS'
-    ],
-    languagesTitle: 'Idiomas',
-    languages: [{
-        language: 'Inglés',
-        level: '(Competencia profesional completa)'
+    aptitudes: {
+        title: 'Principales aptitudes',
+        aptitudes: [
+            'Java',
+            'Javascript/Typescript',
+            'Angular',
+            'React',
+            'HTML',
+            'CSS'
+        ],
     },
-        {
-            language: 'Francés',
-            level: '(Competencia básica)'
+    languages: {
+        title: 'Idiomas',
+        languages: [{
+            language: 'Inglés',
+            level: '(Competencia profesional completa)'
         },
-        {
-            language: 'Español',
-            level: '(Competencia bilingüe o nativa)'
-        },
-        {
-            language: 'Asturiano',
-            level: '(Competencia bilingüe o nativa)'
-        }],
-    certificationsTitle: 'Certificaciones',
-    certifications: [
-        {
-            name: `Udacity's React Nanodegree`,
-            resources: '../assets/react_nanodegree.pdf'
-        },
-        {
-            name: 'Shaping up with Angular.js',
-        },
-        {
-            name: 'Web Assessment for SAP Message Processor and Operations for Support Consultant',
-        },
-    ],
-    certificationsInProgressTitle: 'Certificaciones en progreso',
-    certificationsInProgress: [
-        {
-            name: 'Epic React',
-            resources: 'https://epicreact.dev/'
-        },
-    ],
-    honorsTitle: 'Logros',
-    honors: ['Premio mejor expediente promoción 2009-2011 de Máster de Ingeniería Web'],
+            {
+                language: 'Francés',
+                level: '(Competencia básica)'
+            },
+            {
+                language: 'Español',
+                level: '(Competencia bilingüe o nativa)'
+            },
+            {
+                language: 'Asturiano',
+                level: '(Competencia bilingüe o nativa)'
+            }],
+    },
+    certifications: {
+        title: 'Certificaciones',
+        certifications: [
+            {
+                name: `Udacity's React Nanodegree`,
+                resources: '../assets/react_nanodegree.pdf'
+            },
+            {
+                name: 'Shaping up with Angular.js',
+            },
+            {
+                name: 'Web Assessment for SAP Message Processor and Operations for Support Consultant',
+            },
+        ],
+    },
+    certificationsInProgress: {
+        title: 'Certificaciones en progreso',
+        certificationsInProgress: [
+            {
+                name: 'Epic React',
+                resources: 'https://epicreact.dev/'
+            },
+        ],
+    },
+    honors: {
+        title: 'Logros',
+        honors: ['Premio mejor expediente promoción 2009-2011 de Máster de Ingeniería Web'],
+    },
     job: 'Desarrolladora de software',
-    extractTitle: 'Extracto',
-    extract: 'Desarrolladora full stack con perfil Java en la parte Back y CSS, HTML, Javascript/Typescript y Angular en la parte Front. ' +
-            'Me gustan los retos y aprender cosas nuevas de continuo. En cuanto tengo un momento libre me pongo a dibujar y pintar, ' +
-            'puede ser por eso que muchas veces siento que la balanza se me inclina a la parte Front.',
-    experienceTitle: 'Experiencia',
-    experience: [
-        {
-            company: 'Empathy.co',
-            role: 'Desarrolladora front-end',
-            duration: 'June 2020 - Present'
-        },
-        {
-            company: 'Zapiens Technologies SL',
-            role: 'Analista programadora',
-            duration: 'Mayo de 2017 - June 2020'
-        },
-        {
-            company: 'Ricoh España',
-            role: 'Programadora Web Senior',
-            duration: 'Abril de 2015 - Mayo de 2017'
-        },
-        {
-            company: 'Nortia Inteligencia de Negocio',
-            role: 'Analista programadora',
-            duration: 'Junio de 2011 - Abril de 2015'
-        },
-        {
-            company: 'Grupo Iris',
-            role: 'Programadora Web',
-            duration: 'Setiembre de 2010 - Junio de 2011'
-        },
-        {
-            company: 'Universidad de Oviedo',
-            role: 'Webmaster',
-            duration: 'Setiembre 2009 - Julio 2010'
-        }
-    ],
-    educationTitle: 'Educación',
-    education: [
-        {
-            place: 'Universidad de Oviedo',
-            degree: 'Master de Ingeniería Web (2009 - 2011)'
-        },
-        {
-            place: 'Universidad de Oviedo',
-            degree: 'Ingeniería Técnica en Informática de Sistemas (2005 - 2009)'
-        }
-    ]
+    extract: {
+        title: 'Extracto',
+        extract: 'Desarrolladora full stack con perfil Java en la parte Back y CSS, HTML, Javascript/Typescript y ' +
+                'Angular en la parte Front. ' +
+                'Me gustan los retos y aprender cosas nuevas de continuo. En cuanto tengo un momento libre me pongo a dibujar y pintar, ' +
+                'puede ser por eso que muchas veces siento que la balanza se me inclina a la parte Front.',
+    },
+    experience: {
+        title: 'Experiencia',
+        experience: [
+            {
+                company: 'Empathy.co',
+                role: 'Desarrolladora front-end',
+                duration: 'June 2020 - Present'
+            },
+            {
+                company: 'Zapiens Technologies SL',
+                role: 'Analista programadora',
+                duration: 'Mayo de 2017 - June 2020'
+            },
+            {
+                company: 'Ricoh España',
+                role: 'Programadora Web Senior',
+                duration: 'Abril de 2015 - Mayo de 2017'
+            },
+            {
+                company: 'Nortia Inteligencia de Negocio',
+                role: 'Analista programadora',
+                duration: 'Junio de 2011 - Abril de 2015'
+            },
+            {
+                company: 'Grupo Iris',
+                role: 'Programadora Web',
+                duration: 'Setiembre de 2010 - Junio de 2011'
+            },
+            {
+                company: 'Universidad de Oviedo',
+                role: 'Webmaster',
+                duration: 'Setiembre 2009 - Julio 2010'
+            }
+        ],
+    },
+    education: {
+        title: 'Educación',
+        education: [
+            {
+                place: 'Universidad de Oviedo',
+                degree: 'Master de Ingeniería Web (2009 - 2011)'
+            },
+            {
+                place: 'Universidad de Oviedo',
+                degree: 'Ingeniería Técnica en Informática de Sistemas (2005 - 2009)'
+            }
+        ],
+    },
 };
